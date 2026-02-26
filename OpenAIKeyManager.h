1cce23e2915043e8d06125eef4f0404ee19530ce
i0s_tweak3r <djvs23@gmail.com>
2026-02-26 18:46:18 -0500

    Refactor OpenAIKeyManager.h for preference management
    Signed-off-by: i0s_tweak3r <djvs23@gmail.com>

--- OpenAIKeyManager.h
+++ OpenAIKeyManager.h
@@ -4,30 +4,32 @@
 //
 //  Created by Brian A Nooning on 3/19/23.
 //
-
-
 #ifndef OpenAIKeyManager_h
 #define OpenAIKeyManager_h
 
 #endif
 
-// OpenAIKeyManager_h
+    // OpenAIKeyManager_h
 
-#import <UIKit/UIKit.h>
-#import <Foundation/Foundation.h>
-#include <stdlib.h>
-#import <objc/runtime.h>
+    #import <UIKit/UIKit.h>
+    #import <Foundation/Foundation.h>
+    #include <stdlib.h>
+    #import <objc/runtime.h>
 
-#define Prefs [[NSUserDefaults alloc] initWithSuiteName:@"com.i0stweak3r.ezcomplete"]
+
+#define Prefs [NSUserDefaults standardUserDefaults]
     
-#define Prefs_setObjectForKey(objectToSet, key) [Prefs setObject:objectToSet forKey:key]
+#define Prefs_setObject(object,key) [Prefs setObject:object forKey:key]
 //Used for converting type if necessary,
 //like Prefs_objectForKey:string [NSString stringWithFloat:string] floatValue];
-#define Prefs_objectForKey(key) [Prefs objectForKey:key]   //returns any object in appropiate id form, so @"1" could be a float,integer, or char stored as NSNumber object
+#define Prefs_getObject(key) [Prefs objectForKey:key]   //returns any object in appropiate id form, so @"1" could be a float,integer, or char stored as NSNumber object
 #define Prefs_getString(key) [Prefs stringForKey:key]   //returns string value for supplied key thats saved in defaults
-#define Prefs_setStringForKey(string, key) [Prefs setObject:string forKey:key]  //set a string value for a specific key aka set KVP
-#define Prefs_setIntegerForKey(integer, key) [Prefs setInteger:integer forKey:key]   //set integer to specific key
+#define Prefs_setString(string, key) [Prefs setObject:string forKey:key]  //set a string value for a specific key aka set KVP
+#define Prefs_setInteger(integer, key) [Prefs setInteger:integer forKey:key]   //set integer to specific key
 #define Prefs_getInteger(key) [Prefs integerForKey:key]   //get integer from a key that holds an integer value
+#define Prefs_setFloat(float,key) [Prefs setFloat:float forKey: key]
+
+//#define Prefs_getFloat(key) [Prefs floatForKey:key]   // returns a float from a key that holds a float value
 
 @interface OpenAIKeyManager : NSObject
 - (NSString *) promptUserForKey;
